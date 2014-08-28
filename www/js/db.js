a7910e0add43162d28b73c029361ceffce31829c
kbevemyr <katrin@bevemyr.com>
2014-08-28 21:23:42 +0200

    dev

--- www/js/db.js
+++ www/js/db.js
@@ -2,3 +2,19 @@ function event(user) {
     return {status: "ja", evenemang: "SAYO (dag 2)", date: "2014/06/06"};
 }
 
+function get_all_users(sid, cont) {
+    alert("in function");
+//return [{user: "johan", email: "jb@bevemyr.com", tel: "070123"}];
+    $.ajax({
+        url: "http://idrott.bevemyr.com/idrott/get_all_users?sid="+sid,
+	dataType: "json",
+	success: function(data) {
+	       cont(data.users);
+	},
+	error: function(data) {
+	    alert(data.status);
+	    $.mobile.changePage($("#login"));  // should be errorpage
+	}
+    });
+
+}
