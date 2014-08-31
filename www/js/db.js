ac6f87695fcf9c549e963238254379e428f01fe1
kbevemyr <katrin@bevemyr.com>
2014-08-31 11:59:43 +0200

    temp checkin

--- www/js/db.js
+++ www/js/db.js
@@ -18,3 +18,28 @@ function get_all_users(sid, cont) {
     });
 
 }
+
+function layout_all_users(users) {
+  for (var i = 0; i < users.length;  i++) {
+    var row = $('<tr>');
+    row.appendTo("#admin-funclist-table");
+    row.append($('<td>').append(users[i].name));
+    row.append($('<td>').append(users[i].group));
+    row.append($('<td>').append(users[i].tel));
+    row.append($('<td>').append(users[i].email));
+    row.append($('<td>').append(users[i].comment));
+  }
+}
+
+function layout_all_reqs(reqs) {
+/*
+          <li class="ui-field-contain">
+            <label for="flip2">Flip switch:</label>
+            <select name="flip2" id="flip2" data-role="slider">
+                <option value="off">Off</option>
+                <option value="on">On</option>
+            </select>
+        </li>
+   Tanken är att använda detta för bekräftelse av event.
+*/
+}
