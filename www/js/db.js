f1c21ac3300a8ec75d29cd7d610576459a629151
katrin <katrin@bevemyr.com>
2014-09-21 19:52:31 +0200

    connect user to event loop

--- www/js/db.js
+++ www/js/db.js
@@ -32,7 +32,7 @@ function layout_all_users(sid, tablename, users) {
     row.append($('<td>').append(users[i].tel));
     row.append($('<td>').append(users[i].email));
     row.append($('<td>').append(users[i].comment));
-    row.append($('<td>').append(x));
+    //row.append($('<td>').append(x));
   }
   //alert("Groups: "+groups);
   get_all_events(sid, layout_select_event);
