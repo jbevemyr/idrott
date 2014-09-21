c87fb343f64af34dade84b074ce4b5f9488a1f62
katrin <katrin@bevemyr.com>
2014-09-21 23:17:08 +0200

    connect user to event loop

--- www/js/db.js
+++ www/js/db.js
@@ -24,7 +24,6 @@ function layout_all_users(sid, tablename, users) {
   x = "<button onclick=\"select_event('" + users[i].username + "')\" data-iconpos=\"notext\" data-icon=\"bullets\" data-transition=\"pop\"></button>";
  //   x = $('<div>');
  //      x = $('<button data-icon="bullets" data-iconpos="notext" data-transition="pop">').button().button('refresh');
-    x.button("refresh");
 
     if (groups.indexOf((users[i].group) < 0) || (users[i].group != "")) {
       groups.push(users[i].group);
