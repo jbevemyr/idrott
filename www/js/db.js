834d271c5dc25c74857799415d4d8b4936938f50
katrin <katrin@bevemyr.com>
2014-09-24 18:49:11 +0200

    uncheck radiobutton

--- www/js/db.js
+++ www/js/db.js
@@ -65,7 +65,7 @@ function layout_select_event(events) {
 
 function connect_user_to_event(sid, username) {
     var eid = $('input[name=admin-add-user-to-event-event]:checked').val();
-    $('input[name=admin-add-user-to-event-event]').removeProp('checked');
+    $('input[name=admin-add-user-to-event-event]').removeAttribute('checked');
 
     get_named_user(sid, username, eid, function(data) {
         var newEventList = data.user.events;
