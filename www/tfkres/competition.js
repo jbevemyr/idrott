33eb19f426a45e9e852e8216ddc09eb449f699af
katrin <katrin@bevemyr.com>
2014-11-23 00:38:44 +0100

    lite nya former på presentationen

--- www/tfkres/competition.js
+++ www/tfkres/competition.js
@@ -48,24 +48,21 @@ $(function () {
     var NavView = Backbone.View.extend({
         el: $("#nav"),
 
-        resultTemplate: _.template($('#nav-list-template').html()),
+        resultTemplate: _.template($('#nav-grouplist-template').html()),
 
         render: function () {
             var groups = [],
                 headplace = $(this.el),
                 resulttemplate = this.resultTemplate;
 
-            //console.log("collection: "+this.collection.toJSON());
             var grouplist = this.collection.map(function (x) {
                 return x.toJSON().group;
             });
             grouplist = _.unique(grouplist);
-            //console.log(grouplist);
 
             for (var i=0; i < grouplist.length; i++)
             {
                 var group = grouplist[i];
-                console.log(group);
 
                 var subgrouplist = this.collection.filter(function (x) {
                     return x.toJSON().group == group;
@@ -86,17 +83,16 @@ $(function () {
                 subgroupfilter = _.reduce(subgroupfilter, function (memo, x) {
                     return memo + x;
                 });
-                console.log(subgroupfilter);
 
                 var groupfilter = group + " " + subgroupfilter;
 
                 groups.push({group: group, groupfilter: groupfilter, subgroups: subgroupstringlist});
-                console.log("group: "+group+", groupfilter: "+groupfilter+", subgroups: "+subgroupstringlist[2]);
+                //console.log("group: "+group+", groupfilter: "+groupfilter+", subgroups: "+subgroupstringlist[2]);
             }
 
             var navstruct = resulttemplate({groups: groups});
             //var navstruct = resulttemplate({group: "TESTGROUP", groupfilter: "TESTGROUPFILTER", subgroup: "TESTSUBGROUP"});
-            console.log(navstruct);
+            //console.log(navstruct);
             headplace.append(navstruct);
             return this;
         }
